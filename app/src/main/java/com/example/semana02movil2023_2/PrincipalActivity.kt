3343892f05b219789c7cff78b332544bbb55d09c
renzovargas04 <20100038@ue.edu.pe>
2023-09-09 09:40:24 -0500

    He agregado el Principal Activity, continuar

---
+++ app/src/main/java/com/example/semana02movil2023_2/PrincipalActivity.kt
@@ -0,0 +1,14 @@
+package com.example.semana02movil2023_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+        //Este es mi codigo
+
+
+    }
+}
